1f105a3f1f0843b67eb7c8bb5e238eade2eb3f3e
Cristina Moreira <cristinamoreira2510@gmail.com>
2023-02-28 19:35:57 -0300

    Funciones de botones en js

---
+++ js/proyecto.js
@@ -0,0 +1,24 @@
+document.getElementById('boton1').onclick = function(){
+   console.log ("sobremi");
+document.getElementById('sobremi').style.display='block';
+}
+
+document.getElementById('boton2').onclick = function(){
+    console.log ("educacion");
+ document.getElementById('educacion').style.display='block';
+ }
+
+ document.getElementById('boton3').onclick = function(){
+    console.log ("experiencia");
+ document.getElementById('experiencia').style.display='block';
+ }
+
+ document.getElementById('boton4').onclick = function(){
+    console.log ("habilidades");
+ document.getElementById('habilidades').style.display='block';
+ }
+
+ document.getElementById('boton5').onclick = function(){
+    console.log ("certif");
+ document.getElementById('certif').style.display='block';
+ }
